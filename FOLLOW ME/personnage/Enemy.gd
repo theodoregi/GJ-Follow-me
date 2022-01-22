d7af1b29bf08deb0c705ee7aa1dfb0cfc510ef2a
TheodoreMagicarpe <t.gigault@gmail.com>
2022-01-22 14:30:40 +0100

    personnage v2

--- FOLLOW ME/personnage/Enemy.gd
+++ FOLLOW ME/personnage/Enemy.gd
@@ -8,15 +8,32 @@ onready var _animated_sprite_attack = $Attack_enemy
 const GRAVITY = 3000.0
 var velocity = Vector2.ZERO
 const UP = Vector2(0, -1)
+enum {MOVE, ATTACK,DEATH}
+
+var state=MOVE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if state==MOVE:
+		idle_enemy()
+	elif state==ATTACK:
+		death_enemy()
+
+func idle_enemy():
 	_animated_sprite_idle.show()
 	_animated_sprite_death.hide()
-	_animated_sprite_attack.hide()	
+	_animated_sprite_attack.hide()
 	_animated_sprite_idle.play()
-	
-	
+
+func death_enemy():
+	_animated_sprite_idle.hide()
+	_animated_sprite_death.show()
+	_animated_sprite_death.play()
+	_animated_sprite_attack.hide()
+	if _animated_sprite_death.frame==10: 
+		queue_free()
+
+
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	velocity=move_and_slide(velocity,UP)
@@ -27,9 +44,9 @@ func _physics_process(delta):
 #	pass
 
 
-func _death_enemy_area_entered(area):
-	pass # Replace with function body.
-
-
 func _attack_enemy_area_entered(area):
-	pass # Replace with function body.
+	state=ATTACK
+
+
+func _death_enemy_area_entered(area):
+	state=DEATH
--- FOLLOW ME/personnage/personnage.gd
+++ FOLLOW ME/personnage/personnage.gd
@@ -12,9 +12,9 @@ onready var _timer = $Timer
 enum {MOVE, ATTACK,DEATH}
 
 const GRAVITY = 3000
-const WALK_SPEED = GRAVITY/12
+const WALK_SPEED = 300
 const WALK_ATTACK = WALK_SPEED/3
-const JUMP_HIGH = WALK_SPEED * 2.5
+const JUMP_HIGH = GRAVITY/5
 var state=MOVE
 var velocity = Vector2.ZERO
 const UP = Vector2(0,-1)
@@ -29,13 +29,13 @@ func _physics_process(delta):
 	velocity.x =WALK_SPEED
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y=-JUMP_HIGH
-	if !is_on_floor():
-		jump_character(delta)
-	elif state==ATTACK or Input.is_action_just_pressed("attack") and is_on_floor():
-		_attack(delta)
-	elif state==DEATH: 
+	if state==DEATH:
 		velocity.x=0
 		death_character(delta)
+	elif !is_on_floor():
+		jump_character(delta)
+	elif state==ATTACK or (Input.is_action_just_pressed("attack") and is_on_floor()) :
+		_attack(delta)
 	else:
 		move_character(delta)
 	velocity=move_and_slide(velocity,UP)
@@ -58,8 +58,6 @@ func jump_character(delta):
 	_animated_sprite_death.hide()
 	_animated_sprite_attack.hide()
 	_animated_sprite_idle.hide()
-	
-
 
 func _death_area_entered(area):
 	state=DEATH
@@ -67,12 +65,12 @@ func _death_area_entered(area):
 func death_character(delta):
 	_animated_sprite_run.hide()
 	_animated_sprite_jump.hide()
-	_animated_sprite_death.show()
-	_animated_sprite_attack.hide()
-	_animated_sprite_death.play()
 	_animated_sprite_idle.hide()
-	
-	
+	_animated_sprite_attack.hide()
+	_animated_sprite_death.show()
+	_animated_sprite_death.play()
+	if _animated_sprite_death.frame==10: 
+		queue_free()
 
 func _attack(delta):
 	velocity.x =WALK_ATTACK
@@ -87,21 +85,8 @@ func _attack(delta):
 		_animated_sprite_attack.frame = 0
 		_timer.start(0.5)
 
-
 func _on_Timer_timeout():
 	state=MOVE
 
-
 func _attack_area_entered(area):
 	pass # Replace with function body.
-
-
-func _on_RayCast2D_tree_entered():
-	velocity.x =WALK_ATTACK
-	_animated_sprite_run.hide()
-	_animated_sprite_run.hide()
-	_animated_sprite_jump.hide()
-	_animated_sprite_death.hide()
-	_animated_sprite_attack.hide()
-	_animated_sprite_idle.show()
-	_animated_sprite_idle.play()
